1cd6048a7fbb836bbf25f0882384f9601f02355d
DaviJuvino <davijuvinosouza@gmail.com>
2025-03-12 22:11:52 -0300

    Add bidirecional relacionamento

---
+++ SpringApplication/springdata/relacionamentos/OneToMany/Bidirecional/spring-boot-one-to-many-master/src/main/java/com/techgeeknext/model/Employees.java
@@ -0,0 +1,27 @@
+package com.techgeeknext.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employees {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organization_id")
+    @JsonBackReference
+    private Organization organization;
+
+}
