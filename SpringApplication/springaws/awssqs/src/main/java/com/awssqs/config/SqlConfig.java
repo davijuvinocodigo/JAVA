5888118ab6314013968452320ce53caf8c4490ff
davi <davijuvinosouza@gmail.com>
2025-11-15 14:08:09 -0300

    .

---
+++ SpringApplication/springaws/awssqs/src/main/java/com/awssqs/config/SqlConfig.java
@@ -0,0 +1,20 @@
+package com.awssqs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+import java.net.URI;
+
+@Configuration
+public class SqlConfig {
+
+    @Bean
+    public SqsAsyncClient sqsAsyncClient() {
+        return SqsAsyncClient.builder()
+                .region(Region.US_EAST_1)
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .build();
+    }
+}
---
+++ SpringApplication/springaws/awssqs/src/main/java/com/awssqs/consumer/MyMessage.java
@@ -0,0 +1,4 @@
+package com.awssqs.consumer;
+
+public record MyMessage(String content) {
+}
